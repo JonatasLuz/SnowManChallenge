35c07803814a639e484306b9bb4ef55245a4710b
Jonatas da Luz <jdl.canaisdcd@scopus.com.br>
2021-01-26 18:22:22 -0300

    first commit

---
+++ SnowManChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnowManChallenge
+//
+//  Created by Jonatas da Luz on 26/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
