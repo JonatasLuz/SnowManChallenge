c8f607223ae081cb84406ccea4538b089b3dd7b9
Jonatas da Luz <jdl.canaisdcd@scopus.com.br>
2021-01-28 16:27:15 -0300

    Created UIColor extension

---
+++ SnowManChallenge/Resources/UIColorExtension.swift
@@ -0,0 +1,25 @@
+//
+//  UIColorExtension.swift
+//  SnowManChallenge
+//
+//  Created by Jonatas da Luz on 28/01/21.
+//
+import UIKit
+
+extension UIColor {
+    static func darkBlue() -> UIColor {
+        return UIColor(red: 18/255, green: 30/255, blue: 152/255, alpha: 1)
+    }
+    
+    static func goldenYellow() -> UIColor {
+        return UIColor(red: 254/255, green: 189/255, blue: 44/255, alpha: 1)
+    }
+    
+    static func lightPink() -> UIColor {
+        return UIColor(red: 253/255, green: 113/255, blue: 119/255, alpha: 1)
+    }
+    
+    static func lightGreen() -> UIColor {
+        return UIColor(red: 77/255, green: 200/255, blue: 168/255, alpha: 1)
+    }
+}
