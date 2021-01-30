cbaac8244bbe8115c8978c940548f041354c97b4
Jonatas da Luz <jdl.canaisdcd@scopus.com.br>
2021-01-30 17:15:39 -0300

    changed width of asnwer view

--- SnowManChallenge/Views/QuestionTableViewCell.swift
+++ SnowManChallenge/Views/QuestionTableViewCell.swift
@@ -23,7 +23,7 @@ class QuestionTableViewCell: UITableViewCell {
         iconImageView.image = UIImage(systemName: "chevron.down")
         self.addSubview(iconImageView)
         
-        answerLabel = UILabel(frame: CGRect(x: 15, y:  self.frame.height + 10, width: self.frame.width * 0.85, height: 100))
+        answerLabel = UILabel(frame: CGRect(x: 15, y:  self.frame.height + 10, width: self.frame.width * 0.9, height: 100))
         answerLabel.numberOfLines = 0
         
         answerLabel.isHidden = false
